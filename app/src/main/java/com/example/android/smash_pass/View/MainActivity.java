58664e2dd4a5010aa57c5ec5222975aa9b477ad8
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-04-24 15:13:32 +0200

    Added Background Image

--- app/src/main/java/com/example/android/smash_pass/View/MainActivity.java
+++ app/src/main/java/com/example/android/smash_pass/View/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.smash_pass.Model.MyObserver;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         setupViewModel();
     }
+
 
     private void setupViewModel() {
         viewModel = new ViewModel();
