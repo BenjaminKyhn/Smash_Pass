56f81200d5a6584dc47d8f92200d4c1454003669
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-24 20:21:46 +0200

    Added a TextView with a title and an ImageView with an image from a URL to VideoGameActivity

--- app/src/main/java/com/example/android/smash_pass/View/VideoGameActivity.java
+++ app/src/main/java/com/example/android/smash_pass/View/VideoGameActivity.java
@@ -3,14 +3,21 @@ package com.example.android.smash_pass.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.android.smash_pass.R;
+import com.squareup.picasso.Picasso;
 
 public class VideoGameActivity extends AppCompatActivity {
+    ImageView mainImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_game);
+
+        mainImage = findViewById(R.id.mainImage);
+        String pictureUrl = "https://www.pngitem.com/pimgs/m/157-1579710_picture-chuck-norris-hd-png-download.png";
+        Picasso.get().load(pictureUrl).into(mainImage);
     }
 }
