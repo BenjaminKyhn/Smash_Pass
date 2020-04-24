9a20583a7abb5ebb1407482ac3a6b2e050289750
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-24 13:27:03 +0200

    MainActivity.java can now change to other activities

--- app/src/main/java/com/example/android/smash_pass/View/MainActivity.java
+++ app/src/main/java/com/example/android/smash_pass/View/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.smash_pass.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +15,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btOne;
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
     private ViewModel viewModel;
 
     @Override
@@ -21,7 +27,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btOne = findViewById(R.id.btnOne);
+        btnOne = findViewById(R.id.btnOne);
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToFightingGames();
+            }
+        });
+
+        btnTwo = findViewById(R.id.btnTwo);
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToFPSGames();
+            }
+        });
+
+        btnThree = findViewById(R.id.btnThree);
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToMMOGames();
+            }
+        });
+
+        btnFour = findViewById(R.id.btnFour);
+        btnFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToRTSGames();
+            }
+        });
+
+        btnFive = findViewById(R.id.btnFive);
+        btnFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToPuzzlePlatformGames();
+            }
+        });
 
         setupViewModel();
     }
@@ -33,5 +77,30 @@ public class MainActivity extends AppCompatActivity {
             public void update(Object o) {
             }
         });
+    }
+
+    public void changeToFightingGames(){
+        Intent myIntent = new Intent(this, FightingGamesActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void changeToFPSGames(){
+        Intent myIntent = new Intent(this, FPSGamesActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void changeToMMOGames(){
+        Intent myIntent = new Intent(this, FPSGamesActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void changeToRTSGames(){
+        Intent myIntent = new Intent(this, FPSGamesActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void changeToPuzzlePlatformGames(){
+        Intent myIntent = new Intent(this, FPSGamesActivity.class);
+        startActivity(myIntent);
     }
 }
