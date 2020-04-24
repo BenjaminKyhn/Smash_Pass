ed13da85d4025305d0e7155bedec6ccd98061388
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-24 12:46:12 +0200

    Added ViewModel

---
+++ app/src/main/java/com/example/android/smash_pass/View/ViewModel.java
@@ -0,0 +1,23 @@
+package com.example.android.smash_pass.View;
+
+import com.example.android.smash_pass.Model.MyObserver;
+import com.example.android.smash_pass.Model.VideoGame;
+import com.example.android.smash_pass.Persistence.FirebaseHandler;
+
+public class ViewModel {
+    private VideoGame videoGame;
+    private FirebaseHandler firebaseHandler;
+
+    public ViewModel() {
+        videoGame = new VideoGame();
+        firebaseHandler = new FirebaseHandler(videoGame);
+    }
+
+    public void observeVideoGame(MyObserver myObserver) {
+        videoGame.addObserver(myObserver);
+    }
+
+    public void persist(String text) {
+        firebaseHandler.insert(text);
+    }
+}
