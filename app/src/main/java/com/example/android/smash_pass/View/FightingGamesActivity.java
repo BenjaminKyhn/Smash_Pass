654413cd342f6a97ea9fa24e488dfc03aeb66b9d
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-26 19:11:45 +0200

    createButtons() method now uses the HashMap to create the correct amount of buttons and set their text to the name of the video game

--- app/src/main/java/com/example/android/smash_pass/View/FightingGamesActivity.java
+++ app/src/main/java/com/example/android/smash_pass/View/FightingGamesActivity.java
@@ -9,10 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.example.android.smash_pass.Model.VideoGame;
 import com.example.android.smash_pass.R;
 
+import java.util.Map;
+
 public class FightingGamesActivity extends AppCompatActivity {
-    ViewModel viewModel;
+    private ViewModel viewModel;
+    private Map<String, VideoGame> fightingGamesMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,44 +25,54 @@ public class FightingGamesActivity extends AppCompatActivity {
 
         // Instantiate a view model so we can get the map of video games
         viewModel = new ViewModel();
+        fightingGamesMap = viewModel.getFightingGamesMap();
 
         // Create a list of buttons and add them to the activity
         createButtons();
     }
 
-    public void back(View view){
+    public void back(View view) {
         Intent myIntent = new Intent(this, MainActivity.class);
         startActivity(myIntent);
     }
 
-    public void changeToVideoGame(View view){
+    public void changeToVideoGame(View view) {
         Intent myIntent = new Intent(this, VideoGameActivity.class);
         startActivity(myIntent);
     }
 
-    public void createButtons(){
+    public void createButtons() {
         LinearLayout linearLayout = findViewById(R.id.fightingGamesLinearLayout);
 
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
-        params.setMargins(0,40,0,0);
+        params.setMargins(0, 40, 0, 0);
 
-        Button myButton = new Button(this);
+        for (String key : fightingGamesMap.keySet()) {
+            // Create the video game object
+            VideoGame videoGame = fightingGamesMap.get(key);
 
-        myButton.setLayoutParams(params);
-        myButton.setTextSize(20);
-        myButton.setTextColor(Color.WHITE);
-        myButton.setBackgroundColor(Color.parseColor("#6200EE"));
+            // Create a button
+            Button myButton = new Button(this);
 
-        myButton.setText("Test");
+            // Set the appearance of the button
+            myButton.setLayoutParams(params);
+            myButton.setTextSize(20);
+            myButton.setTextColor(Color.WHITE);
+            myButton.setBackgroundColor(Color.parseColor("#6200EE"));
+            String gameTitle = videoGame.getName();
+            myButton.setText(gameTitle);
 
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+            // Give the button an on-click method
+            myButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
 
-        linearLayout.addView(myButton);
+            // Add the button to the activity as a ButtonView
+            linearLayout.addView(myButton);
+        }
     }
 }
