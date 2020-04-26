5606785481bd75c27e392294af29376207e58a9a
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-26 18:56:00 +0200

    Added a method for creating ButtonViews with java code

--- app/src/main/java/com/example/android/smash_pass/View/FightingGamesActivity.java
+++ app/src/main/java/com/example/android/smash_pass/View/FightingGamesActivity.java
@@ -3,8 +3,11 @@ package com.example.android.smash_pass.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.android.smash_pass.R;
 
@@ -14,6 +17,8 @@ public class FightingGamesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fighting_games);
+
+        createButtons();
     }
 
     public void back(View view){
@@ -24,5 +29,25 @@ public class FightingGamesActivity extends AppCompatActivity {
     public void changeToVideoGame(View view){
         Intent myIntent = new Intent(this, VideoGameActivity.class);
         startActivity(myIntent);
+    }
+
+    public void createButtons(){
+        LinearLayout linearLayout = findViewById(R.id.fightingGamesLinearLayout);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0,40,0,0);
+
+        Button myButton = new Button(this);
+
+        myButton.setLayoutParams(params);
+        myButton.setTextSize(20);
+        myButton.setTextColor(Color.WHITE);
+        myButton.setBackgroundColor(Color.parseColor("#6200EE"));
+
+        myButton.setText("Test");
+
+        linearLayout.addView(myButton);
     }
 }
