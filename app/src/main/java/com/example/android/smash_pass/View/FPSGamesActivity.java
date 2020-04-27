775b58883157ece05f9385e477a8e59add59de75
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-04-27 10:03:05 +0200

    FPSGamesActivity Opdaterede

--- app/src/main/java/com/example/android/smash_pass/View/FPSGamesActivity.java
+++ app/src/main/java/com/example/android/smash_pass/View/FPSGamesActivity.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.android.smash_pass.Model.VideoGame;
 import com.example.android.smash_pass.R;
 
+import java.util.Map;
+
 public class FPSGamesActivity extends AppCompatActivity {
+    private ViewModel viewModel;
+    private Map<String, VideoGame> fpsGamesMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_f_p_s_games);
+
+        viewModel = new ViewModel();
+        fpsGamesMap = viewModel.getfpsGamesMap();
     }
 
     public void back(View view){
@@ -25,7 +33,20 @@ public class FPSGamesActivity extends AppCompatActivity {
     public void changeToVideoGame(View view, VideoGame videoGame) {
         Intent myIntent = new Intent(this, VideoGameActivity.class);
         startActivity(myIntent);
+        VideoGameActivity.videoGame = videoGame;
     }
 
+    public void createButton() {
+        LinearLayout linearLayout = findViewById(R.id.fpsGamesLinearLayout);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 40, 0, 0);
+
+        for (String key : fpsGamesMap.keySet()) {
+
+        }
+    }
 
 }
